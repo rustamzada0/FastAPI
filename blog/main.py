9465582598c473fe055219cc072762cffa4d20c2
Tunay <rustemzade.tunay.2000@gmail.com>
2023-10-12 00:33:05 +0400

    get/post/put/delete

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
-from . import models, database, schemas, crud
+from . import models, database, schemas
 
 app = FastAPI()
 
@@ -19,7 +19,26 @@ def blogapp():
     return 'salam'
 
 
-@app.post('/blogapp')
+@app.get('/blogs')
+def get_all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blogs).all()
+    print(blogs)
+    return blogs
+
+
+@app.get('/blog/{id}', status_code=200)
+def show_blog(id: int, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blogs).filter(models.Blogs.id == id).first()
+    print(blog)
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"is not available")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail': 'is not available'}
+    return blog
+
+
+@app.post('/blogapp', status_code=status.HTTP_201_CREATED)
 def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     print(request)
     new_blog = models.Blogs(title = request.title, body = request.body)
@@ -29,18 +48,20 @@ def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blogs')
-def get_all(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blogs).all()
-    print(blogs)
-    return blogs
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(get_db)):
+    print(db)
+    db.query(models.Blogs).filter(models.Blogs.id == id).delete(synchronize_session=False)
+    db.commit()
+    return 'done'
 
 
-@app.get('/blog/{id}')
-def show_blog(id: int, db: Session = Depends(get_db)):
-    blog = db.query(models.Blogs).filter(models.Blogs.id == id).first()
-    print(blog)
-    return blog
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    print(request)
+    db.query(models.Blogs).filter(models.Blogs.id == id).update(request)
+    db.commit()
+    return 'update!'
 
 
 @app.on_event("startup")
